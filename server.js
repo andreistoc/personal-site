904b19ec822184846255f918ec75383bbe3a530a
Andrei Istoc <andreistoc@yahoo.com>
2018-05-23 11:17:13 +0300

    Hopefully added the ability to use all files

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 
 var app = express();
 
+app.use('/', express.static(__dirname + '/'))
+
 app.get('/', function(req,res) {
   res.sendFile(process.cwd() + '/index.html');
 });
