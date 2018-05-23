fa2f9c7061b98470c6565c54e26f483a9405279b
Andrei Istoc <andreistoc@yahoo.com>
2018-05-23 10:32:51 +0300

    Updated server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = express();
 
 app.get('/', function(req,res) {
   res.sendFile(process.cwd() + '/index.html');
-}
+});
 
 app.listen(80, function() {
   console.log('Listening on port 80...')
-})
+});
